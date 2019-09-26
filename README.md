5e3d913ba8639a9a7e55b81f6fa6402e7d0668f3
Evan Brecht-Curry <starfoxz13@gmail.com>
2019-09-26 07:28:15 -0700

    adding supporting docs

---
+++ README.md
@@ -0,0 +1,21 @@
+# LAB - 31
+
+ ## Hooks
+
+ ### Author: Evan Brecht-Curry
+
+ ### Links and Resources
+* [submission PR](https://github.com/evan-401-advanced-javascript/lab-32-practice/pull/1)
+* [travis](https://www.travis-ci.com/evan-401-advanced-javascript/lab-32-practice)
+* [netlify](https://vibrant-bohr-5a11e3.netlify.com/) 
+* [docs]( http://localhost:6060/)
+
+ ##### Exported Values and Methods
+
+### Setup
+* `npx styleguidist server`
+
+#### `.env` requirements
+
+ #### UML
+![UML](uml.png)
--- client/src/hooks/useForms.jsx
+++ client/src/hooks/useForms.jsx
@@ -1,24 +1,25 @@
-import {useState} from 'react';
-import Q from "@nmq/q/client";
-import io from 'socket.io-client';
-const socket = io.connect('http://localhost:3003');
+import { useState } from 'react';
 
+/**
+ * function handle the submit event
+ * @param callback
+ * @returns {[{}, handleChange, handleSubmit]}
+ */
 export default function useForm(callback) {
-
   const [values, setValues] = useState({});
 
   const handleChange = e => {
-    setValues({...values, [e.target.name]: e.target.value});
+    setValues({ ...values, [e.target.name]: e.target.value });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     e.target.reset();
 
-    if(callback) {
+    if (callback) {
       callback(values);
     }
   };
 
-    return [values, handleChange, handleSubmit]
+  return [values, handleChange, handleSubmit];
 }
